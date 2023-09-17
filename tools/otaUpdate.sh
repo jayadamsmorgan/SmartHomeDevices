5d9e2783bd84b2507f95c2ea1f9b73c39650cb3c
jayadamsmorgan <applejay1802@gmail.com>
2023-09-17 18:33:59 -0400

    scripts update

---
+++ tools/otaUpdate.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+pio run -d $SCRIPT_DIR/../Devices/Home$1
+
+python3 $SCRIPT_DIR/espota.py -i 192.168.1.$2 -a admin -f Devices/Home$1/.pio/build/esp32-s3-devkitc-1/firmware.bin -r -t 3
+
